b7aa2df2194395b66773864901a79fed2e7f69cd
EmmaJWalker <ewalker1@ucsc.edu>
2024-03-12 15:27:51 -0400

    removed all the old code that hadn't been updates in this project in forever

--- at_equilibrium_rootfunc.R
+++
@@ -1,7 +0,0 @@
-#stopping function for when the change in patch occupancy is less than 1e-4
-#and therefore the SRLM is essentially at equilibrium
-
-at.equilibrium <- function(t, p, parameters) {
-  dP <- unlist(SRLM.ODE(t, p, parameters))
-  Norm(dP) - 1e-4
-}
